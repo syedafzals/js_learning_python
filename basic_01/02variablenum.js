ecb801ed6b6819d78aa58c2b71011cf729988c21
syedafzals <syedafzals110@gmail.com>
2026-02-24 05:01:06 +0000

    create varible file

---
+++ basic_01/02variablenum.js
@@ -0,0 +1,4 @@
+var number = 42.2
+age = 30
+addr=1234
+console.log("num\t"+number,"\nage"+age, "\naddr"+typeof(addr));
---
+++ basic_01/02variablestring.js
@@ -0,0 +1,10 @@
+let name = 'Alice';
+var age = 30;
+var city = "New York";
+
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("City: " + typeof city);
+
+name = "Bob"; // Reassigning name variable
+console.log("Updated Name: " + name);
