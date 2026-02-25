561116241f98eb477be86d5290844aaf6c8f7ba7
syedafzals <syedafzals110@gmail.com>
2026-02-25 04:59:50 +0000

    variable datatype and minproject

---
+++ basic_01/minvariableproject.js
@@ -0,0 +1,8 @@
+const id=1234;
+let name ="ahmed"
+let age =23
+let gender;
+let batch="python web"
+
+// console.log("id "+id,"\nname "+name, "\nage "+age,"\nG "+gender,"\nBatch "+batch)
+console.table(["id"+id,name,age+"age",gender,batch])
--- basic_01/test.js
+++ basic_01/test.js
@@ -1,1 +1,9 @@
-console.log("hello world")
+const names = "hello pakistan";
+age=13.34;
+// names ="zabtech"  //update name value
+console.log(names)
+console.log(names)
+console.log(typeof(names))
+console.log(typeof age)
+console.log(age)
+console.log(age)
