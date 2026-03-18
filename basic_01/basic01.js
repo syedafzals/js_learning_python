3c96fe548c7a91f38a43be1c17f851e067ba089a
syedafzals <syedafzals110@gmail.com>
2026-03-18 04:20:41 +0000

    updata

--- basic_01/basic01.js
+++ basic_01/basic01.js
@@ -1,1 +1,7 @@
-console.error("hello world")
+console.log("hello/\\\\\/\\\nworld/\\")
+console.log(2+2)
+let name={"name":"ali","age":21,"class":"python","Gender":"M"};
+const _Name=name;
+
+console.table(_Name)
+
---
+++ basic_01/numberandmath.js
@@ -0,0 +1,24 @@
+// const num= 1200;
+// console.log(num);
+name="11233"
+// const num =new Number(name)
+// console.log( num)
+// const balance =(250/650)*100;
+// console.log(balance.toString().length)
+// console.log(balance)
+// console.log(balance.toFixed(4))
+// const value=10200990;
+// console.log(value.toLocaleString('en-IN'))
+// const amount=123.384544543;
+// console.log(amount.toPrecision(4))
+// ++++++++++++++++++++++Math++++++++++++++++++++
+console.log(Math);
+console.log(Math.abs(-44))
+console.log(Math.round(3.9434))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.8))
+console.log(Math.min(2,3,5,6,1,0))
+console.log(Math.max(2,3,5,6,8))
+
+console.log(Math.random().toFixed(0))
+console.log(Math.random()*10 +1)
