dc8c8b2b10797e66b89d757de1643e7baa587463
syedafzals <syedafzals110@gmail.com>
2026-04-06 05:42:38 +0000

    function start

---
+++ basic3/function.js
@@ -0,0 +1,20 @@
+// function sayname(){
+//     console.log("S");
+//     console.log("a");
+//     console.log("a");
+//     console.log("d") 
+    
+// }
+function name(){
+    console.log("Hello");
+}
+// sayname()
+name()
+name()
+name()
+function sum() {
+    return 2 + 2;
+}
+// Function Parameters
+
+console.log(sum())
