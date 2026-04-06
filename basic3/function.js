384b783907517d68de679ecbe3d83f17a41eb348
syedafzals <syedafzals110@gmail.com>
2026-04-06 06:09:20 +0000

    Function Parameters

--- basic3/function.js
+++ basic3/function.js
@@ -9,12 +9,28 @@ function name(){
     console.log("Hello");
 }
 // sayname()
+function person(name,batch) {
+    return `Name______${name} 
+    Batch_______${batch}
+    `
+    
+}
+let sdt1= person("Ali","Python")
+let sdt2 = person("Saad","Python")
+
+console.log(sdt1)
+console.log(sdt2)
 name()
 name()
 name()
+
 function sum() {
     return 2 + 2;
 }
 // Function Parameters
+function sub(num1,num2) {
+    return num1-num2;
+}
+su=sub(12,4)
+console.log(su)
 
-console.log(sum())
