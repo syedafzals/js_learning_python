53641a5846cda5b19d08dcd2ad2f233bafecba88
syedafzals <syedafzals110@gmail.com>
2026-04-07 06:13:05 +0000

    hello

---
+++ if.js
@@ -0,0 +1,20 @@
+let name =123
+
+if(name==="123"){
+    console.log(`welcome ${name}`)
+}else if (name==123) {
+    console.log(`welcome to ${name}`)
+    
+}
+else{
+    console.log("invalid")
+}
+
+// function sum(num1,op,num2){
+//     if (op==="+"){
+//         return num1+num2
+//     }
+// }
+
+// let cal=sum(2,"+",3)
+// console.log(cal)
